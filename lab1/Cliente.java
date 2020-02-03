e09fabf6d5ef6612e2b94373612fb6bc0f37f126
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-02-03 21:30:31 +0000

    Add files via upload

---
+++ lab1/Cliente.java
@@ -0,0 +1,13 @@
+package lab1;
+import java.util.*;
+public class Cliente{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Ponto n=new Ponto(sc.nextDouble(),sc.nextDouble());
+        Ponto m=new Ponto(sc.nextDouble(),sc.nextDouble());
+        sc.close();
+        double res=n.dist(m);
+        System.out.print(Math.floor(res));
+
+    }
+}
