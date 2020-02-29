2a6290dd0b170934802dff6fbe7c7c644913d78c
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-02-29 19:18:06 +0000

    Update Pirate.java

--- lab3/Pirate.java
+++ lab3/Pirate.java
@@ -10,27 +10,7 @@ public class Pirate {
         groups(a);
     }
 
-    private void groups(char[] a){
-        int enime=0,allie=0;
-        for(int i=0;i<a.length;i++){
-            if(i%2!=0){
-                allies[allie++]=a[i]-'0';
-            }else{ 
-                enimes[enime++]=a[i]-'0';
-            }
-        }
-    }
 
-    public int sum(){
-        int e=0,a=0,i=0,j=0;
-        while(e<=a && i<allies.length){
-            e+=enimes[j++];
-            a+=allies[i++];
-        }
-        if(i<allies.length){a=a-allies[i-=1];}
-        if(a+e>9999) System.exit(0);
-        return a+e;
-    }
     
 
 }
