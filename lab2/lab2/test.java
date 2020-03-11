8159eaf9e7b3b0d013de766e10bfbb9d40568919
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-03-11 21:32:34 +0000

    new

---
+++ lab2/lab2/test.java
@@ -0,0 +1,9 @@
+import lab2.lab2;
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class test {
+    @Test
+    public void testConstructor0() {
+    assertEquals(0, 0); }
+    
+}
