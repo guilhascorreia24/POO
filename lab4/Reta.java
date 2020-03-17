1cf5c41a50fea45dd0835a39fc10b3fb042add5f
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-03-17 15:13:21 +0000

    master

--- lab4/Reta.java
+++ lab4/Reta.java
@@ -1,7 +1,7 @@
 public class Reta {
     private double declive;
     private double ordenada=0;
-    private double vertical=0;
+    private double vertical=Double.NEGATIVE_INFINITY;
 
     public Reta(double m, double b) {
         this.declive = m;
@@ -13,10 +13,16 @@ public class Reta {
     }
 
     public Ponto intersecao(Reta r1) {
-
+        if(!Double.isInfinite(vertical)){
+            ordenada=vertical;
+        }
 	    double x = (r1.ordenada - this.ordenada) / (this.declive - r1.declive);
-	    double y = this.declive * x + this.ordenada;
-
+        double y = this.declive * x + this.ordenada;
+        if(!Double.isInfinite(vertical)){
+            double s=x;
+            y=x;
+            x=s;
+        }
 	    return new Ponto( x,  y);
     }
 
